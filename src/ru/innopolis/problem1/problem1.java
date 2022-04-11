8b5173354cd2b0984b3bcb488cf14e840004cc74
glebuben <90417655+glebuben@users.noreply.github.com>
2022-04-11 19:33:48 +0300

    Update problem1.java

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,80 @@
-// your solution here
-//fix all the errors including package errors
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class Problem1 {
+    public static void main(String[] args) {
+    }
+}
+
+interface Handler{
+    void setNext(Handler handler);
+    void handle(String request, List<String> logs);
+}
+
+class FileWriter implements Handler{
+    PrintWriter writer;
+    private Handler next;
+    public FileWriter(String fileName, String encoding) throws FileNotFoundException, UnsupportedEncodingException {
+        writer = new PrintWriter(fileName, encoding);
+    }
+    @Override
+    public void setNext(Handler handler) {
+        this.next = handler;
+    }
+    @Override
+    public void handle(String request, List<String> logs) {
+        if(request.equals("File")){
+            for (String log:
+                 logs) {
+                writer.println(log);
+            }
+        }
+        else {
+            next.handle(request, logs);
+        }
+    }
+}
+
+class ConsoleWriter implements Handler{
+    private Handler next;
+    @Override
+    public void setNext(Handler handler) {
+        this.next = handler;
+    }
+    @Override
+    public void handle(String request, List<String> logs) {
+        if(request.equals("Console")){
+            for (String log:
+                    logs) {
+                System.out.println(log);
+            }
+        }
+        else {
+            next.handle(request, logs);
+        }
+    }
+}
+
+class ServerSender implements Handler{
+    private Handler next;
+    @Override
+    public void setNext(Handler handler) {
+        this.next = handler;
+    }
+    @Override
+    public void handle(String request, List<String> logs) {
+        if(request.equals("Server")){
+            for (String log:
+                    logs) {
+                //send to server
+            }
+        }
+        else {
+            if(next != null){
+                next.handle(request, logs);
+            }
+        }
+    }
+}
